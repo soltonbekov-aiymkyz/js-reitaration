c5360903785b92610378f29e5748d9c65de91efc
Your Name <you@example.com>
2021-05-31 21:12:26 -0700

    addded new files

---
+++ scripts.js
@@ -0,0 +1,22 @@
+//1st item 
+// function myFunction(){
+//     var popup = document.getElementById('myPopup');
+//     popup.classList.toggle("show");
+// }
+
+
+//second Item
+// function myMove(){
+//     let elem = document.getElementById("myAnimation");
+//     let pos = setInterval(frame,10);
+//     function frame(){
+//         if (pos == 350){
+//             clearInterval(id);
+//         }else{
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+            
+//         }
+//     }
+// }
